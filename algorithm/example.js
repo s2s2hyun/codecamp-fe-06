cf224fd234bc7dfcc48d43db9bbc33137d82c531
pre14lim <s2s2hyun0601@gmail.com>
2022-03-14 18:37:20 +0900

    첫 잔디

---
+++ algorithm/example.js
@@ -0,0 +1,24 @@
+let answer = 0 // 이동횟수
+let current = 1; // 로봇의 현재 위치 
+
+                 
+answer = answer+1
+current = urrent+2   반복
+                
+
+if( current !== 100) {
+    current = current+1 
+    answer =answer +1 
+}
+let answer = 0;   // 이동횟수 
+const limit = 100;  // 이동할 층
+
+for( let i = 1; i<limit; i=i+2) {
+        answer = answer +1;
+}
+
+
+// 
+const limit =100  // 이동할층
+
+constanswer = math.floor( limi /2 )
---
+++ class/pages/01-02-emotion/index.js
@@ -0,0 +1,19 @@
+import { MyTitle, MyEmail } from '../../styles/emotion'
+
+
+
+
+export default function AAApage() {
+
+
+
+  //여기는 자바스크립트 쓰는곳 //
+  
+  
+    return (
+        <MyTitle>
+            안녕하세요
+            <input type="text"/>
+        </MyTitle>
+  )
+}
