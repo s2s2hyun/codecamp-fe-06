67b6cef3a4788f7ba3b42bfd7d03824d0b01566f
pre14lim <s2s2hyun0601@gmail.com>
2022-05-05 15:13:56 +0900

    목요일 낮

--- class_build/next.config.js
+++ class_build/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
     reactStrictMode: true,
     trailingSlash: true,
+    generateBuildId: () => "codecamp-6",
+    exportPathMap: () => ({
+        "/": { page: "/" },
+        "/boards": { page: "/boards" },
+        "/404": { page: "/404" },
+    }),
 };
 
 module.exports = nextConfig;
